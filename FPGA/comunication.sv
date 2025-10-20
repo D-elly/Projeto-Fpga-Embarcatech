61ccc5d85d75f4f9c4406f137cf55fc82802f374
D-elly <danielly.s@acad.ifma.edu.br>
2025-10-19 21:04:59 -0300

    terminado testbanch. Tempo de execução e passagem de valores aceitáveis, testar depois como mandar através do testbanch um arquivo de audio com os valores binários

--- FPGA/comunication.sv
+++ FPGA/comunication.sv
@@ -1,11 +1,13 @@
+`timescale 1ns / 1ps
+
 module comunication #(
     parameter int unsigned clock_max = 25_000_000
 )(
-    input logic clk_in,
-    input wire sclk_in, //clock vindo do pico
-    input wire mosi_in, //input data from pico 
-    input wire active,
-    input wire reset,
+    input logic clk_25mhz,
+    input logic sclk_in, //clock vindo do pico
+    input logic mosi_in, //input data from pico 
+    input logic active,
+    input logic reset,
 
     output logic [15:0] audio_out,  //testar se pacotes de 16 bits fazem uma transnmissão lisa
     output logic data_ready
@@ -23,14 +25,14 @@ logic [3:0]  bit_counter;
 logic sclk_d1, sclk_d2; 
 
 //tornando o clock do pico um clock conhecido pelo fpga
-always_ff @(posedge clock_max) begin 
+always_ff @(posedge clk_25mhz) begin 
         sclk_d1 <= sclk_in;
         sclk_d2 <= sclk_d1;
 end 
 
 assign sclk_posedge = (sclk_d1 == 1'b1) && (sclk_d2 == 1'b0);
 
-always_ff @(posedge clock_max or posedge reset)
+always_ff @(posedge clk_25mhz or posedge reset)
     begin
         if(reset == 1'b1) begin  //implementação do botão de desligar ou desativar comunicação
             state <= IDLE;
@@ -40,15 +42,14 @@ always_ff @(posedge clock_max or posedge reset)
         
 
         end else begin //a cada pulso de clock do pico, executar
-            data_ready <= 1'b0;
+            if (data_ready == 1'b1) begin
+                data_ready <= 1'b0;
+            end
 
             case(state)
                 IDLE: begin //modo ociosos, sem novos dados chegando
-                    if(active == 1'b0) begin 
-                        state <= IDLE;
-                        data_ready <= 1'b0;
-
-                    end else if(active == 1'b1) begin
+            
+                    if(active == 1'b1) begin
                         state <= RECEIVING;
                         shift_reg <= 16'b0000000000000000;
                         bit_counter <= 4'b0000;
@@ -57,14 +58,14 @@ always_ff @(posedge clock_max or posedge reset)
 
                 RECEIVING: begin //estado em que a comunicação está acontecendo, recebendo dados do pico
                     if(active == 1'b0) begin
-                        state = IDLE;
+                        state <= IDLE;
+                        
                     end else if (sclk_posedge) begin
                         shift_reg <= {shift_reg[14:0], mosi_in};
 
                         if(bit_counter == 4'd15) begin
                             audio_out <= {shift_reg[14:0], mosi_in};
                             state <= RECEIVING;
-                            shift_reg <= 16'b0000000000000000;
                             data_ready <= 1'b1;
                             bit_counter <= 4'b0000;
 
