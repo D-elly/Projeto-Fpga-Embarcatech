a42823fd2b2e51813ed3cee5d23692ae3de8785c
D-elly <danielly.s@acad.ifma.edu.br>
2025-10-25 23:48:13 -0300

    Implementação de dac_driver.sv, ainda fazendo testes com testbanch, integrando com módulo top.

--- FPGA/dac_driver.sv
+++ FPGA/dac_driver.sv
@@ -1,65 +1,105 @@
 module dac_driver #( 
     parameter int unsigned clock_max = 25_000_000
 )(
-    //recepção de dados de modulos eff
-
-    input logic data_ready, 
-    input logic [15:0] mosi_in,
+    //recepção de dados de audio(modificado ou original)
+    input logic clk_25mhz,
+    input logic data_ready, //trigger para sincronização de clock com outros módulo
+    input logic [15:0] mosi_in, //audio de entrada
+    input logic reset, //apenas para dar start e integrar com outros módulo auxilares
 
     //pinos do protocolo de comunicação com modulo dac fisico
     output logic sclk_out, 
-    output logic [11:0] miso_out, 
-    output logic active_out
+    output logic mosi_out, //mandando bit a bit para slave spi
+    output logic active_out,
+    input logic miso_out //util p/ debbug, verificar se dac está convertendo certo
 );
 
+//modos da FSM p/ master SPI
+typedef enum logic {
+    DAC_IDLE, 
+    DAC_TRANSFER
+    //DAC_RECEIVING  //opcional, util para debug
+}state_t;
 
-assign miso_out = mosi_in[15:4];
+state_t state;
 
-always_ff @(posedge clk_25mhz or posedge reset)
-    begin
-        if(reset == 1'b1) begin  //implementação do botão de desligar ou desativar comunicação
-            state <= IDLE;
-            shift_reg <= 16'b0000000000000000;
-            bit_counter <= 4'b0000;
-            data_ready <= 1'b0;
-            audio_out <= 16'b0000000000000000;
-        
+//geração do clock para comunicação com slave SPI, tecnica vista em sala
+localparam int sclk_div_count = 5; //para clock de 2mhz
+logic [$clog2(sclk_div_count) -1:0] count_clock = '0; //var de tamanho de 2Mhz, para contar pulsos
+logic internal_clock = 1'b0;
 
-        end else begin //a cada pulso de clock do pico, executar
-            if (data_ready == 1'b1) begin
-                data_ready <= 1'b0;
+logic [15:0] shift_reg; 
+logic [3:0]  bit_counter;
+logic busy;
+
+always_ff @(posedge clk_25mhz or posedge reset) begin
+    if(reset) begin
+        count_clock <= '0;
+        internal_clock <= 1'b0;
+
+    end else if(state == DAC_TRANSFER) begin
+        if(count_clock == sclk_div_count) begin  //contador de clock interno atingido
+            count_clock <= '0;
+            internal_clock <= ~internal_clock;
+
+        end else begin  //continuar incrementação
+            count_clock <= count_clock + 1;
+        end
+    end else begin  //se manter em modo DAC_IDLE
+        count_clock <= '0;
+        internal_clock <= 1'b0;
+    end 
+end
+
+assign sclk_out = internal_clock;
+
+//FSM para controle principal do master e slave SPI
+always_ff @(posedge clk_25mhz or posedge reset) begin
+    if(reset) begin 
+        state <= DAC_IDLE;
+        active_out <= 1'b1;
+        mosi_out <= 1'b0;
+        busy<= 1'b0;
+        bit_counter <= 4'b0000;
+    end
+
+    busy <= 1'b0;
+    case (state)
+        DAC_IDLE: begin
+            active_out <= 1'b1;
+            mosi_out <= 1'b0;
+            busy <= 1'b0;
+
+            if(data_ready) begin 
+                state <= DAC_TRANSFER;
+                
+                shift_reg <= {1'b1, 3'b000, mosi_in[15:4]};  //configurando mensagem p/ SPI slave com cabeçalo e truncamento de dados para saída
+                bit_counter <= 4'b0000;
+                active_out <= 1'b0;
+                busy <= 1'b1;
+            end 
+        end
+
+        DAC_TRANSFER: begin 
+            busy <= 1'b1;
+            active_out <= 1'b0;
+
+            if(count_clock == sclk_div_count) begin 
+                if(bit_counter == 4'd15) begin 
+                    mosi_out <= shift_reg[15];
+                    shift_reg <= shift_reg << 1; //desliza bits, do maior para menor
+                    bit_counter <= bit_counter + 1;
+                
+                end else begin //após mandar uma amostra, volta para DAC_IDLE e espera próximo sinal data_ready
+                    state <= DAC_IDLE;
+                    active_out <= 1'b1;
+                    busy <= 1'b0;
+                end
             end
 
-            case(state)
-                IDLE: begin //modo ociosos, sem novos dados chegando
-                    if(active == 1'b1) begin
-                        state <= RECEIVING;
-                        shift_reg <= 16'b0000000000000000;
-                        bit_counter <= 4'b0000;
-                    end
-                end  
+        end
+    endcase        
+end
 
-                RECEIVING: begin //estado em que a comunicação está acontecendo, recebendo dados do pico
-                    if(active == 1'b0) begin
-                        state <= IDLE;
-                        
-                    end else if (sclk_posedge) begin
-                        shift_reg <= {shift_reg[14:0], mosi_in};
-
-                        if(bit_counter == 4'd15) begin
-                            audio_out <= {shift_reg[14:0], mosi_in};
-                            state <= RECEIVING;
-                            data_ready <= 1'b1;
-                            bit_counter <= 4'b0000;
-
-                        end else begin
-                            bit_counter <= bit_counter + 1'b1;
-                        end
-                    end
-                end 
-
-            endcase
-        end 
-    end 
 
 endmodule
--- FPGA/top.sv
+++ FPGA/top.sv
@@ -4,15 +4,16 @@ module top#(
 )(
     //entradas globais
     input logic clk_25mhz, reset, 
+    input logic mode_sound, //um botão que ativa o audio modificado,  
 
     //pinos de entrada de dados SPI, comunication.sv
     input logic com_sclk_in, com_mosi_in, com_active,
 
     //pinos de saida de dados do dac_driver.sv
     output logic spi_audio_clk, 
-    output logic [15:0] spi_mosi_out, 
+    output logic spi_mosi_out, 
     output logic spi_active_out,
-    output logic [11:0] spi_miso_out
+    input logic  spi_miso_out
 );
 
 logic data_is_ready;  //sinal interno do top-level
@@ -40,19 +41,24 @@ logic modified_status; //armazena se aplicação de efeito terminou
             .process_status(modified_status)
         );
 
-//passagem da faixa de audio, original ou modificada, 
-assign output_audio = (reset)?  modified_audio: original_audio;
+//passagem da faixa de audio, original ou modificada,
+mode_sound = 1'b0; 
+assign output_audio = (mode_sound)?  modified_audio: original_audio;
 
 //copia modulo dac_driver
     dac_driver #(.clock_max(clock_max) 
         )u_dac_driver(
             //entradas do fpga -> dac
+            .clk_25mhz(clk_25mhz),
             .data_ready(data_is_ready),
             .mosi_in(output_audio),
+            .reset(reset),
 
             //saidas do dac -> amplificador
-            .sclk_out(spi_audio_clk), .miso_out(spi_miso_out), 
-            .active_out(spi_active_out)
+            .sclk_out(spi_audio_clk), 
+            .mosi_out(spi_mosi_out),
+            .active_out(spi_active_out), 
+            .miso_out(spi_miso_out) 
     );
 
 
